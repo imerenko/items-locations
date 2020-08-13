b4635eb5e6da173e441db3eb1ea17e7a2811154e
imerenko <imerenko@gmail.com>
2020-08-13 10:41:28 -0400

    flex repository

--- src/main/java/com/my/repository/FlexRepository.java
+++ src/main/java/com/my/repository/FlexRepository.java
@@ -10,6 +10,9 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * Flex repository
+ */
 @Repository
 public class FlexRepository {
 
